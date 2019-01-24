a324a157eb53231fd3338f631fff01b3bcdecbdf
TrungNT <nickartin13@gmail.com>
2019-01-24 11:29:42 +0200

    test

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements = [
 ]
 
 links = [
-    'https://github.com/imito/odin/tarball/master#egg=odin-0.1.0',
+    'git+https://github.com/imito/odin/tarball/master#egg=odin-0.1.0',
 ]
 
 setup(
